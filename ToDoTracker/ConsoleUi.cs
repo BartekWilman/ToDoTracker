f6dfef4f7ecdb2974e26302803e059fa48878613
Bartek Wilman <bartekwilman@op.pl>
2025-12-26 20:06:55 +0100

    Refraktor ConsoleUI

--- ToDoTracker/ConsoleUi.cs
+++ ToDoTracker/ConsoleUi.cs
@@ -8,6 +8,114 @@ namespace ToDoTracker
 {
     internal static class ConsoleUi
     {
+        //przefiltrowanie listy
+        public static IEnumerable<TodoItem> List(TodoService service,TaskStatus? status = null)
+        {
+            if (status == null)
+            {
+                return service.GetList();
+            }
+            return service.GetList().Where(x => x.Status == status.Value);
+        }
+        //wypisanie elementów listy z numeracją
+        public static void ShowList(IEnumerable<TodoItem> items)
+        {
+            int i = 1;
+            foreach (var item in items)
+            {
+                Console.WriteLine($"[{i}]");
+                item.ShowInfo();
+                i++;
+            }
+        }
+        //wyświetlanie przefiltrowanych list
+        public static void GiveMeLists(TodoService serv)
+        {
+            IEnumerable<TodoItem> list;
+            Console.Clear();
+            Console.WriteLine("1. Wszystkie\n2. Aktywne\n3. Ukończone\nPokaz mi: ");
+            string show = Console.ReadLine();
+            switch (show)
+            {
+                case "2":
+                    list = List(serv, TaskStatus.Active);
+                    break;
+                case "3":
+                    list = List(serv, TaskStatus.Done);
+                    break;
+                default:
+                    list = List(serv);
+                    break;
+            }
+            ShowList(list);
+            Console.ReadKey();
+        }
+        //stworzenie TodoItem i dodanie go do listy (atrybutu)
+        public static void MakeAndAdd(TodoService serv)
+        {
+            Console.Clear();
+            Console.Write("Podaj tytul zadania: ");
+            string title = Console.ReadLine();
+            string pri;
+            Priority priorytet;
+            while (true)
+            {
+                Console.WriteLine("Podaj priorytet\n1. Niski\n2. Sredni\n3. Wysoki");
+                pri = Console.ReadLine();
+
+                if (pri == "1") { priorytet = Priority.Low; break; }
+                if (pri == "2") { priorytet = Priority.Medium; break; }
+                if (pri == "3") { priorytet = Priority.High; break; }
+
+                Console.WriteLine("Wybierz poziom priorytetu wpisując od 1 do 3");
+            }
+            Console.WriteLine("Konretny termin?\n1. Tak\n2. Nie");
+            string chceszDate = Console.ReadLine();
+            if (chceszDate == "1")
+            {
+                DateTime data = ConsoleUi.GiveMeDate();
+                serv.Add(title, priorytet, data);
+            }
+            else
+            {
+                serv.Add(title, priorytet);
+            }
+        }
+        private static bool PickItemAndExecute(
+            TodoService serv,
+            bool allowDecline,
+            string prompt,
+            Action<TodoItem> action,
+            TaskStatus? status = null)
+        {
+            var list = List(serv, status).ToList();
+            if (list.Count == 0)
+            {
+                Console.WriteLine("Lista nie zawiera elementów");
+                return false;
+            }
+            ShowList(list);
+            TodoItem[] array = list.ToArray();
+            Console.WriteLine(prompt);
+            var input = Console.ReadLine();
+            if (allowDecline)
+            {
+                if (input == "0")
+                {
+                    Console.WriteLine("Anulowano");
+                    return false;
+                }
+
+            }
+            if (!int.TryParse(input, out int n) || n < 1 || n > array.Length)
+            {
+                Console.WriteLine("Nieprawidłowy numer.");
+                Console.ReadKey();
+                return false;
+            }
+            action(array[n - 1]);
+            return true;
+        }
         public static DateTime GiveMeDate()
         {
             Console.Write("Podaj rok: ");
@@ -21,7 +129,7 @@ namespace ToDoTracker
         }
         public static void ShowMenu()
         {
-            Console.WriteLine("TO DO TRACKER\n1. Pokaz zadania\n2. Dodaj zadanie\n3. Ukończ zadanie\n4. Zakończ działanie programu");
+            Console.WriteLine("TO DO TRACKER\n1. Pokaz zadania\n2. Dodaj zadanie\n3. Ukończ zadanie\n4. Usuń zadanie\n0. Zakończ działanie programu");
         }
         public static string Prompt()
         {
@@ -35,8 +143,8 @@ namespace ToDoTracker
         public static void MainLoop()
         {
             var service = new TodoService();
-            var staorage = new TodoStorage();
-            LoadFromFile(staorage, service );
+            var storage = new TodoStorage();
+            LoadFromFile(storage, service );
             bool flag = true;
             string prompt;
             while (flag)
@@ -48,17 +156,32 @@ namespace ToDoTracker
                 switch (prompt)
                 {
                     case "1":
-                        service.GiveMeLists();
+                        GiveMeLists(service);
                         break;
                     case "2":
-                        service.MakeAndAdd();
-                        staorage.Save(service);
+                        MakeAndAdd(service);
+                        storage.Save(service);
                         break;
                     case "3":
-                        service.CompleteTaskFromList(); 
-                        staorage.Save(service);
+                        PickItemAndExecute(
+                            service,
+                            allowDecline: true,
+                            prompt: "Podaj numer zadania do ukończenia (0 = anuluj): ",
+                            action: item => service.MarkDone(item.Id),
+                            status: TaskStatus.Active
+                            );
+                        storage.Save(service);
                         break;
                     case "4":
+                        bool del = PickItemAndExecute(
+                            service,
+                            allowDecline: true,
+                            prompt: "Podaj numer zadania do usunięcia",
+                            action: item => service.RemoveItem(item.Id)
+                            );
+                        if (del) storage.Save(service);
+                        break;
+                    case "0":
                         Console.WriteLine("NARA ;)");
                         Console.ReadKey();
                         flag = false;
--- ToDoTracker/Services/TodoService.cs
+++ ToDoTracker/Services/TodoService.cs
@@ -9,13 +9,16 @@ namespace ToDoTracker
 {
     internal class TodoService
     {
+        //lista Toditem
         private List<TodoItem> _items = new List<TodoItem>();
+        //KONSTRUKTOR
         public TodoItem Add(string title, Priority priority = Priority.Medium, DateTime? due = null)
         {
             var item = new TodoItem(title, priority, due);
             _items.Add(item);
             return item;
         }
+        //odhaczenie jako done
         public bool MarkDone(Guid id)
         {
             var item = _items.FirstOrDefault(x => x.Id == id);
@@ -23,100 +26,21 @@ namespace ToDoTracker
             item.MarkDone();
             return true;
         }
-        public IEnumerable<TodoItem> List(TaskStatus? status = null)
+        //funkcja bool usunięcia TodoItem(Guid Id)
+        public bool RemoveItem(Guid id)
         {
-            if (status == null)
-            {
-                return _items;
-            }
-            return _items.Where(x => x.Status == status.Value);
+            var item = _items.FirstOrDefault(x => x.Id == id);
+            if (item == null) return false;
+            _items.Remove(item);
+            return true;
         }
 
-        public void ShowList(IEnumerable<TodoItem> items)
-        {
-            int i = 1;
-            foreach (var item in items)
-            {
-                Console.WriteLine($"[{i}]");
-                item.ShowInfo();
-                i++;
-            }
-        }
-
+        //nadpisanie listy (atrybutu)
         public void LoadItems(List<TodoItem> items)
         {
             _items = items;
         }
-        public void GiveMeLists()
-        {
-            IEnumerable<TodoItem> list;
-            Console.Clear();
-            Console.WriteLine("1. Wszystkie\n2. Aktywne\n3. Ukończone\nPokaz mi: ");
-            string show = Console.ReadLine();
-            switch (show)
-            {
-                case "2":
-                    list = List(TaskStatus.Active);
-                    break;
-                case "3":
-                    list = List(TaskStatus.Done);
-                    break;
-                default:
-                    list = List();
-                    break;
-            }
-            ShowList(list);
-            Console.ReadKey();
-        }
-        public void CompleteTaskFromList()
-        {
-            IEnumerable<TodoItem> list;
-            Console.Clear();
-            list = List(TaskStatus.Active);
-            if (!list.Any()) return;
-            ShowList(list);
-            TodoItem[] array = list.ToArray();
-            Console.Write("Podaj numer zadania, które chcesz odhaczyć: ");
-            var input = Console.ReadLine();
-            if (!int.TryParse(input, out int n) || n < 1 || n > array.Length)
-            {
-                Console.WriteLine("Nieprawidłowy numer.");
-                Console.ReadKey();
-                return;
-            }
-            MarkDone(array[n - 1].Id);
-            Console.ReadKey();
-        }
-        public void MakeAndAdd()
-        {
-            Console.Clear();
-            Console.Write("Podaj tytul zadania: ");
-            string title = Console.ReadLine();
-            string pri;
-            Priority priorytet;
-            while (true)
-            {
-                Console.WriteLine("Podaj priorytet\n1. Niski\n2. Sredni\n3. Wysoki");
-                pri = Console.ReadLine();
-
-                if (pri == "1") { priorytet = Priority.Low; break; }
-                if (pri == "2") { priorytet = Priority.Medium; break; }
-                if (pri == "3") { priorytet = Priority.High; break; }
-
-                Console.WriteLine("Wybierz poziom priorytetu wpisując od 1 do 3");
-            }
-            Console.WriteLine("Konretny termin?\n1. Tak\n2. Nie");
-            string chceszDate = Console.ReadLine();
-            if (chceszDate == "1")
-            {
-                DateTime data = ConsoleUi.GiveMeDate();
-                Add(title, priorytet, data);
-            }
-            else
-            {
-                Add(title, priorytet);
-            }
-        }
+        //pobranie listy (atrybutu)
         public List<TodoItem> GetList()
         {
             return _items;
